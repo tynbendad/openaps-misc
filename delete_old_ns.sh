33d3d867965ca8bafcd0c091f2db281eb8c13d87
tynbendad <tynbendad@gmail.com>
2019-03-10 12:51:06 -0600

    Update delete_old_ns.sh

--- delete_old_ns.sh
+++ delete_old_ns.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 #
 deletefrom=`date -d "3 years ago" -Iminutes`
-deleteto=`date -d "70 weeks ago" -Iminutes`
-echo "deleting entries..."
-echo "deletefrom=$deletefrom"
-echo "deleteto=$deleteto"
+deleteto=`date -d "52 weeks ago" -Iminutes`
+echo "deleting entries before $deleteto..."
 curl --compressed -f -m 30 -s -X DELETE -H "API-SECRET: $API_SECRET" "$NIGHTSCOUT_HOST/api/v1/entries/sgv.json?find%5bdateString%5d%5b\$lte%5d=$deleteto&find%5bdateString%5d%5b\$gte%5d=$deletefrom"
-echo "deleting treatments..."
-echo "deletefrom=$deletefrom"
+echo
+echo "deleting treatments before $deleteto..."
 curl --compressed -f -m 30 -s -X DELETE -H "API-SECRET: $API_SECRET" "$NIGHTSCOUT_HOST/api/v1/treatments.json?find%5bcreated_at%5d%5b\$lte%5d=$deleteto&find%5bcreated_at%5d%5b\$gte%5d=$deletefrom"
+echo
 ##
-deleteto=`date -d "26 weeks ago" -Iminutes`
-echo "deleting all but 26 weeks of devicestatus..."
-echo "deletefrom=$deletefrom"
-echo "deleteto=$deleteto"
-#echo 'curl --compressed -f -m 30 -s -X DELETE -H "API-SECRET: $API_SECRET" "$NIGHTSCOUT_HOST/api/v1/devicestatus.json?find%5bcreated_at%5d%5b\$lte%5d=$deleteto&find%5bcreated_at%5d%5b\$gte%5d=$deletefrom"'
+deleteto=`date -d "3 weeks ago" -Iminutes`
+echo "deleting devicestatus before $deleteto..."
 curl --compressed -f -m 30 -s -X DELETE -H "API-SECRET: $API_SECRET" "$NIGHTSCOUT_HOST/api/v1/devicestatus.json?find%5bcreated_at%5d%5b\$lte%5d=$deleteto&find%5bcreated_at%5d%5b\$gte%5d=$deletefrom"
+echo
